abe2325581b59ec3b168eae61700e1b944b58f29
Muskan3909 <166904389+Muskan3909@users.noreply.github.com>
2025-06-11 01:59:13 +0530

    Update task.service.ts

--- frontend/src/app/services/task.service.ts
+++ frontend/src/app/services/task.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Task, TaskFilters } from '../models/task.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
-  private baseUrl = 'http://localhost:5000/api';
+  private baseUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
